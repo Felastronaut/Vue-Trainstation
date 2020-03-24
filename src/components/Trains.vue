9acd719df9f4a63b7ed1926137a625a685cf9bfe
Felastronaut <felix.meynet@master-stic.fr>
2020-03-24 19:32:04 +0100

    First iteration ! After Kenny's tutorial

---
+++ src/components/Trains.vue
@@ -0,0 +1,43 @@
+<template>
+    <b-form>
+        <label class="sr-only" for="depart">Départ</label>
+        <b-input
+            id="depart"
+            placeholder="Départ"
+            v-model="depart"/>
+
+        <label class="sr-only" for="arrivee">Arrivée</label>
+        <b-input 
+            id="arrivee" 
+            placeholder="Arrivée"
+            v-model="arrivee"/>
+
+        <b-button variant="primary" @click="rechercher">Rechercher</b-button>
+        <p>{{voyage}}</p>
+    </b-form>
+</template>
+
+<script>
+export default {
+  name: 'Trains',
+  data() {
+    return{
+        depart: '',
+        arrivee: ''
+    }
+  },
+  methods: {
+      rechercher(){
+          console.log( 'votre voyage est ' + this.voyage)
+      }
+  },
+  computed: {
+      voyage() {
+          return this.depart + ' -> ' + this.arrivee
+      }
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/Formulaire.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        Formulaire
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12">
+        <Trains/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Trains from '@/components/Trains.vue'
+
+export default {
+  name: 'Formulaire',
+  components: {
+    Trains
+  }
+}
+</script>
